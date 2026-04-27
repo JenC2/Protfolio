d0bad19291eecb9805405ce097d4654fa1467d81
jennychen <jenny.chenchou@gmail.com>
2026-04-27 20:04:18 +0800

    Initial

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { words } from "../constants";
+
+const Hero = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-10 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+
+      <div className="hero-layout">
+        {/* LEFT: HERO CONTENT */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                From
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>to shipped software</h1>
+              <p>
+                Hi, I’m Jenny, a full-stack developer focused on building web
+                and mobile applications, from backend systems to clean user
+                experiences.
+              </p>
+            </div>
+          </div>
+        </header>
+        {/* RIGHT: 3D MODEL */}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
