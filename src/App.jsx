c0a185cde5f95c2c1a582e5f73438ff128140b62
jennychen <jenny.chenchou@gmail.com>
2026-05-04 19:04:21 +0800

    implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import ProjectsSection from "./sections/ProjectsSection";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ProjectsSection />
     </>
